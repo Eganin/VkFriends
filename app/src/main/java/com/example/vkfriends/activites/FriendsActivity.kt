02c029517131b401c721400137d50af0f00e8e29
Eganin <egorzaharin12@gmail.com>
2021-02-19 13:36:17 +0300

    add layout frineds list

--- app/src/main/java/com/example/vkfriends/activites/FriendsActivity.kt
+++ app/src/main/java/com/example/vkfriends/activites/FriendsActivity.kt
@@ -1,12 +1,32 @@
 package com.example.vkfriends.activites
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.arellomobile.mvp.MvpAppCompatActivity
+import com.arellomobile.mvp.presenter.InjectPresenter
 import com.example.vkfriends.R
+import com.example.vkfriends.models.FriendModel
+import com.example.vkfriends.presenters.FriendsPresenter
+import com.example.vkfriends.views.FriendsView
 
-class FriendsActivity : AppCompatActivity() {
+class FriendsActivity : MvpAppCompatActivity(),FriendsView {
+
+    @InjectPresenter
+    lateinit var friendsPresenter: FriendsPresenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_friends)
+    }
+
+    override fun showError(text: String) =
+        Toast.makeText(this@FriendsActivity, text, Toast.LENGTH_LONG).show()
+
+    override fun setupEmptyList() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setupFriendsList(friendsList: ArrayList<FriendModel>) {
+        TODO("Not yet implemented")
     }
 }
