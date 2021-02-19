9563e3ad7f785403bde81986666adceea12de6b2
Eganin <egorzaharin12@gmail.com>
2021-02-19 13:22:02 +0300

    implement login view

--- app/src/main/java/com/example/vkfriends/activites/LoginActivity.kt
+++ app/src/main/java/com/example/vkfriends/activites/LoginActivity.kt
@@ -1,12 +1,48 @@
 package com.example.vkfriends.activites
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.vkfriends.R
+import android.widget.Toast
+import androidx.core.view.isVisible
+import com.arellomobile.mvp.MvpAppCompatActivity
+import com.arellomobile.mvp.presenter.InjectPresenter
+import com.example.vkfriends.databinding.ActivityLoginBinding
+import com.example.vkfriends.presenters.LoginPresenter
+import com.example.vkfriends.views.LoginView
 
-class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class LoginActivity : MvpAppCompatActivity(), LoginView {
+
+    private var _binding: ActivityLoginBinding? = null
+
+    private val binding get() = _binding!!
+
+    @InjectPresenter
+    lateinit var loginPresenter: LoginPresenter
+
+    override fun onCreate(savedInstanceState: Bundle?)  {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        _binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            loginPresenter.login(isSuccess = true)
+        }
+    }
+
+    override fun startLoading() {
+        binding.progressBar.isVisible = true
+        binding.loginButton.isVisible = false
+    }
+
+    override fun endLoading() {
+        binding.progressBar.isVisible = false
+        binding.loginButton.isVisible = true
+    }
+
+    override fun showError(text: String) =
+        Toast.makeText(this@LoginActivity, text, Toast.LENGTH_LONG).show()
+
+    override fun openFriends() {
+        startActivity(Intent(this@LoginActivity, FriendsActivity::class.java))
     }
 }
